598f1627bb07db3643196a17ced03df9788eb671
harnoor-mca <harnoor.kalra005@gmail.com>
2026-04-14 02:38:52 +0530

    changes

--- server.js
+++ server.js
@@ -52,8 +52,62 @@ app.get("/api/ngos",(req,res)=>{
     {
         if (err){
             console.log(err);
-            return resstatus(500).json({message:"Error fetching data"});
+            return res.status(500).json({message:"Error fetching data"});
         }
         res.json(result);
     });
+});
+app.get("/api/my-donations/:donor_id",(req,res)=>{
+    const donor_id=req.params.donor_id;
+    // const sql="Select*from donation_request where donor_id=? Order by request_date desc";
+    const sql="Select dr.*,n.organisation_name from donation_request dr join ngo n on dr.ngo_id=n.ngo_id where dr.donor_id=? Order by dr.request_date desc";
+   
+    db.query(sql,[donor_id],(err,result)=>{
+        if (err){
+            console.log(err);
+            return res.status(500).json({message:"Error fetching requests right now"});
+
+        }
+        res.json(result);
+    });
+});
+app.delete("/api/donation/:request_id",(req,res)=>
+{
+    const id=req.params.request_id;
+    const sql="Delete from donation_request where request_id=?";
+    db.query(sql,[id],(err,result)=>{
+        if (err){
+            console.log(err);
+            return res.status(500).json({message:"Error deleting requests right now"});
+        }
+        res.json({message:"Donation request deleted successfully"});
+    });
+});
+app.post("/api/donate", (req, res) => {
+  const {
+    donor_id,
+    ngo_id,
+    donation_type,
+    quantity,
+    message,
+    status,
+    request_date
+  } = req.body;
+
+  const sql = `
+    INSERT INTO donation_request
+    (donor_id, ngo_id, donation_type, quantity, message, status, request_date)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  db.query(sql,
+    [donor_id, ngo_id, donation_type, quantity, message, status, request_date],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Error saving request" });
+      }
+      res.json({ message: "Request sent successfully" });
+    }
+  );
 });
