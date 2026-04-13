72e9fc46a1705d3a51d8bddef1b55c3bfce07e34
Harnoor Kaur <harnoor.kalra005@gmail.com>
2026-04-14 02:57:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,30 +21,6 @@ app.post("/register",(req,res)=>{
         res.json({message:"User registered successfully"});
     });
 });
-// const express=require("express");
-// const cors=require("cors");
-// const app=express();
-// app.use(cors());
-// app.use(express.json());
-
-// const authRoutes=require("./routes/auth");
-// const db = require("./db");
-// app.use("/api",authRoutes);
-// app.listen(5000,()=>{
-//     console.log("Server running on port 5000");
-// });
-// app.post("/register",(req,res)=>{
-//     const {name,email,password,role,phone,city}=req.body;
-//     const sql=`Insert into Donor(name,email,password,role,phone,city) values (?,?,?,?,?,?)`;
-//     db.query(sql,[name,email,password,role,phone,city],(err,result)=> {
-//         if(err){
-//             console.log(err);
-//             return res.status(500).json({message:"Registration failed"});
-//         }
-//         res.json({message:"User registered successfully"});
-//     });
-
-
 app.get("/api/ngos",(req,res)=>{
     const sql="Select *from ngo";
     db.query(sql,(err,result)=>
