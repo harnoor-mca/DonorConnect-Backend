1ed88b2b7d91e23381b6a21816854c435ea96e92
harnoor-mca <harnoor.kalra005@gmail.com>
2026-04-15 18:56:13 +0530

    changed

--- db.js
+++ db.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
 
-// const mysql=require("mysql2");
-// const db=mysql.createConnection({
-//     host:"localhost",
-//     user:"root",
-//     password:"harnoor",
-//     database:"donorconnect"
-// });
-// db.connect((err)=>{
-//     if(err){
-//         console.log("DB error:",err);
-//     } else{
-//         console.log("MySQL connected!");
-//     }
-// });
-
-=======
->>>>>>> 72e9fc46a1705d3a51d8bddef1b55c3bfce07e34
 const mysql=require("mysql2");
 const db=mysql.createConnection({
     host:"localhost",
@@ -31,8 +13,4 @@ db.connect((err)=>{
         console.log("MySQL connected!");
     }
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> 72e9fc46a1705d3a51d8bddef1b55c3bfce07e34
 module.exports=db;
