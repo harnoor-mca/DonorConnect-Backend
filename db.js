7c15a17fde2581ef9958a47c5ce372fa81a55bca
harnoor-mca <harnoor.kalra005@gmail.com>
2026-04-22 03:41:46 +0530

    updated

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const mysql=require("mysql2");
 const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"harnoor",
-    database:"donorconnect"
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT,
+    ssl:{
+        rejectUnauthorized:false
+    }
 });
 db.connect((err)=>{
     if(err){
